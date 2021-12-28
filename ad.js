1010ca380972fa16df0c24a4b0544200486eb2c7
Ramiro Alvarez <ramiro.alvarez@rahcode.com>
2021-12-27 22:44:36 -0600

    0.3.4
    ad.js

---
+++ ad.js
@@ -0,0 +1,21 @@
+// RAH Code
+
+// Ad_Gr_Ho
+let Ad_Gr_Ho = "Lorem";
+document.getElementById("Ad_Gr_Ho_01").innerHTML=Ad_Gr_Ho;
+document.getElementById("Ad_Gr_Ho_02").innerHTML=Ad_Gr_Ho;
+document.getElementById("Ad_Gr_Ho_03").innerHTML=Ad_Gr_Ho;
+document.getElementById("Ad_Gr_Ho_04").innerHTML=Ad_Gr_Ho;
+document.getElementById("Ad_Gr_Ho_05").innerHTML=Ad_Gr_Ho;
+document.getElementById("Ad_Gr_Ho_06").innerHTML=Ad_Gr_Ho;
+
+// Ad_Gr_Cu
+let Ad_Gr_Cu = "Lorem";
+document.getElementById("Ad_Gr_Cu_01").innerHTML=Ad_Gr_Cu;
+document.getElementById("Ad_Gr_Cu_02").innerHTML=Ad_Gr_Cu;
+document.getElementById("Ad_Gr_Cu_03").innerHTML=Ad_Gr_Cu;
+
+// Ad_Gr_Ve
+let Ad_Gr_Ve = "Lorem";
+document.getElementById("Ad_Gr_Ve_01").innerHTML=Ad_Gr_Ve;
+document.getElementById("Ad_Gr_Ve_02").innerHTML=Ad_Gr_Ve;
