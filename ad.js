9dbe3d5e9a768bcf07b81f005aba088c31a4144c
Ramiro Alvarez <ramiro.alvarez@rahcode.com>
2021-12-27 22:57:51 -0600

    0.3.6
    ad.js

--- ad.js
+++ ad.js
@@ -4,18 +4,13 @@
 let Ad_Gr_Ho = "Lorem";
 document.getElementById("Ad_Gr_Ho_01").innerHTML=Ad_Gr_Ho;
 document.getElementById("Ad_Gr_Ho_02").innerHTML=Ad_Gr_Ho;
-document.getElementById("Ad_Gr_Ho_03").innerHTML=Ad_Gr_Ho;
-document.getElementById("Ad_Gr_Ho_04").innerHTML=Ad_Gr_Ho;
-document.getElementById("Ad_Gr_Ho_05").innerHTML=Ad_Gr_Ho;
-document.getElementById("Ad_Gr_Ho_06").innerHTML=Ad_Gr_Ho;
-
 // Ad_Gr_Cu
-let Ad_Gr_Cu = "Lorem";
+let Ad_Gr_Cu = "Lorem02";
 document.getElementById("Ad_Gr_Cu_01").innerHTML=Ad_Gr_Cu;
 document.getElementById("Ad_Gr_Cu_02").innerHTML=Ad_Gr_Cu;
 document.getElementById("Ad_Gr_Cu_03").innerHTML=Ad_Gr_Cu;
 
 // Ad_Gr_Ve
-let Ad_Gr_Ve = "Lorem";
+let Ad_Gr_Ve = "Lorem03";
 document.getElementById("Ad_Gr_Ve_01").innerHTML=Ad_Gr_Ve;
 document.getElementById("Ad_Gr_Ve_02").innerHTML=Ad_Gr_Ve;
