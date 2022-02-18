3599df140be51abe6f98e1a91f6c0d2406418e40
Ramiro Alvarez <ramiro.alvarez@rahcode.com>
2022-02-18 12:26:01 -0600

    New

--- footer.js
+++
@@ -1,5 +0,0 @@
-var jsonstr = '{"copyright":"<center>©2021 - 2022 | RAH Code - All Rights Reserved.</center>"}';
-var activity = JSON.parse(jsonstr);
-var footer = document.getElementById("footer");
-
-footer.innerHTML = activity.copyright;
