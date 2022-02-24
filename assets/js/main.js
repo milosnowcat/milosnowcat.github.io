d487c3fbfdf490d8bdc3071d6f64eaccb407075d
Ramiro Alvarez <ramiro.alvarez@rahcode.com>
2022-02-23 21:36:45 -0600

    footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -199,3 +199,30 @@ themeButton.addEventListener('click', () => {
   localStorage.setItem('selected-theme', getCurrentTheme())
   localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+// Footer
+var footer = document.getElementById('foot')
+page = ''
+names = ''
+
+domain = location.href
+rahcode = 'https://rahcode.com'
+goodidea = 'https://good-idea.rahcode.com'
+
+if (domain.includes(rahcode)) {
+  page = 'RAH Code'
+  names = 'Ramiro Alvarez'
+} else if (domain.includes(goodidea)) {
+  page = 'Good Idea'
+  names = 'RAH Code'
+} else {
+  page = "App"
+  names = "RAH Code"
+}
+
+footer.innerHTML =
+  '<div class="footer__bg"><div class="footer__container container grid"><div><h1 class="footer__title">' +
+  page +
+  '</h1><span class="footer__subtitle">by ' +
+  names +
+  '</span></div><ul class="footer__links"><li><a href="#services" class="footer__link">Services</a></li><li><a href="#portfolio" class="footer__link">Portfolio</a></li><li><a href="#contact" class="footer__link">Contactme</a></li></ul><div class="footer__socials"><a href="https://www.facebook.com" target="_blank" class="footer__social" ><i class="uil uil-facebook-f"></i></a><a href="https://www.instagram.com" target="_blank" class="footer__social"><i class="uil uil-instagram"></i></a><a href="https://twitter.com" target="_blank" class="footer__social"><i class="uil uil-twitter-alt"></i></a></div></div><p class="footer__copy">&#169; RAH Code. All right reserved</p></div>'
